9d9de3b5efdaa822f19fc133cae83fdcb5e140b9
cbaus <github_c@maat-family.de>
2018-11-28 01:15:05 +0900

    added a C code acceleration for pubkey generation that uses libbtc

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import base58, ecdsa
 import cashaddress
 from datetime import datetime, timedelta
 import math
+from subprocess import check_output
+from typing import Optional
 
 _can_decode = True
 _d = hashlib.new('ripemd160')
@@ -12,7 +14,7 @@ def ripemd160(x):
     _d.update(x)
     return _d
 
-def gen_and_compare(cmp: str, anywhere: bool, nposs: int):
+def gen_and_compare(cmp: str, anywhere: bool, libbtc: Optional[str]):
     global _can_decode
     start_time = last_print = datetime.now()
     priv_key_init_byte = os.urandom(32)
@@ -28,9 +30,12 @@ def gen_and_compare(cmp: str, anywhere: bool, nposs: int):
         WIF = base58.b58encode(binascii.unhexlify(fullkey+sha256b[:8]))
 
         # get public key , uncompressed address starts with "1"
-        sk = ecdsa.SigningKey.from_string(priv_key, curve=ecdsa.SECP256k1)
-        vk = sk.get_verifying_key()
-        publ_key = '04' + binascii.hexlify(vk.to_string()).decode()
+        if libbtc: #if libbtc path is given, accelerate with c code
+            publ_key = check_output([libbtc, "-command", "pubfrompriv" , "-p", WIF.decode()])[8:74].decode()
+        else:
+            sk = ecdsa.SigningKey.from_string(priv_key, curve=ecdsa.SECP256k1)
+            vk = sk.get_verifying_key()
+            publ_key = '04' + binascii.hexlify(vk.to_string()).decode()
         hash160 = ripemd160(hashlib.sha256(binascii.unhexlify(publ_key)).digest()).digest()
         publ_addr_a = b"\x00" + hash160
         checksum = hashlib.sha256(hashlib.sha256(publ_addr_a).digest()).digest()[:4]
@@ -65,6 +70,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--search", type=str, help=" e.g. qhi, starts with q/r/z/p", required=True)
     parser.add_argument("-p", "--position", type=int, help="1 if searchString can show up anywhere, 0 for in beginning", default=0)
+    parser.add_argument("-l", "--libbtcpath", type=str, help="to accelerate the pubkey generation with C code point the program to the path where bitcointool is located", default=None)
     args = parser.parse_args()
 
     if len(args.search) == 0:
@@ -82,7 +88,8 @@ if __name__ == "__main__":
     print(f"Vanity address with {nposs} possibilities requested. Prob after many tries is:")
     for tr,pr in zip(tests, prob_after):
         print(f"Tries: {tr:.0} -> {pr}")
-    WIF, btc, bch, i = gen_and_compare(cmp=args.search, anywhere=args.position, nposs=nposs)
+    path_to_libbtc = os.path.join(args.libbtcpath,"bitcointool") if args.libbtcpath else None
+    WIF, btc, bch, i = gen_and_compare(cmp=args.search, anywhere=args.position, libbtc=path_to_libbtc)
     print(f"Checked {i} addresses")
     print("Private Key         : " + WIF)
     print("Bitcoin Address     : " + btc)
